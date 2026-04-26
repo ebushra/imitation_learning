2b0823fd1b913120cb15450752ba6c63b12eb2ed
ebushra <148111952+ebushra@users.noreply.github.com>
2026-04-26 16:33:04 -0700

    Update analyze_acrobot.py training

--- analyze_acrobot.py
+++ analyze_acrobot.py
@@ -113,6 +113,7 @@ model.fit(X_train, y_train)
 # =========================
 
 y_pred = model.predict(X_test)
+y_pred_train = model.predict(X_train)
 
 print("\n=== TEST RESULTS ===")
 print("Accuracy:", accuracy_score(y_test, y_pred))

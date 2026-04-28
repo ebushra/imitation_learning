447e5a2578c5ad1448f826378a5c69b7667bd6a2
ebushra <148111952+ebushra@users.noreply.github.com>
2026-04-28 09:54:33 -0700

    Create train_logreg_mc.py

---
+++ train_logreg_mc.py
@@ -0,0 +1,35 @@
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+
+from data_loader import load_data
+import evaluate
+
+df, X, y = load_data()
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42,
+    stratify=y
+)
+
+scaler = StandardScaler()
+
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+model = LogisticRegression(max_iter=1000)
+
+model.fit(X_train, y_train)
+
+evaluate.run(
+    model,
+    scaler,
+    X_train,
+    X_test,
+    y_train,
+    y_test,
+    df
+)

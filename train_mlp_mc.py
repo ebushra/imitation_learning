62d4d93c965012f8a89ca2b44426b6b943e112c3
ebushra <148111952+ebushra@users.noreply.github.com>
2026-04-28 09:53:58 -0700

    Create train_mlp_mc.py

---
+++ train_mlp_mc.py
@@ -0,0 +1,39 @@
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
+
+from data_loader import load_data
+import evaluate
+
+df, X, y = load_data()
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42,
+    stratify=y
+)
+
+scaler = StandardScaler()
+
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+model = MLPClassifier(
+    hidden_layer_sizes=(64, 64),
+    max_iter=200,
+    random_state=42
+)
+
+model.fit(X_train, y_train)
+
+evaluate.run(
+    model,
+    scaler,
+    X_train,
+    X_test,
+    y_train,
+    y_test,
+    df
+)

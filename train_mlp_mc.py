09ce5e6e18edda899bb227821a1292d026d651cc
ebushra <148111952+ebushra@users.noreply.github.com>
2026-04-28 17:54:58 -0700

    Update train_mlp_mc.py

--- train_mlp_mc.py
+++ train_mlp_mc.py
@@ -2,8 +2,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
 
-from data_loader import load_data
-import evaluate
+from load_data_mc import load_data
+import evaluate_mc
 
 df, X, y = load_data()
 
